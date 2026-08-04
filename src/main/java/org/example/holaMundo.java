c2e11cf1fd364c8ad2633ed74f4067c1af4a2f8c
Salazar@7QWNVV3 <brenda.jsp18@gmail.com>
2026-08-04 15:08:16 -0600

    Se crearon las funciones resta, multiplicaicon y division y se mostró en consola

--- src/main/java/org/example/holaMundo.java
+++ src/main/java/org/example/holaMundo.java
@@ -3,6 +3,11 @@ package org.example;
 public class holaMundo {
     public static void main (String[] args){
         System.out.println("Hola mundo, mi nombre es Brenda Salazar :) ");
+        System.out.println("Nuestras operaciones matematicas creadas son: ");
         System.out.println(operacionesMatematicas.operacionSuma(11,5));
+        System.out.println(operacionesMatematicas.operacionResta(50,5));
+        System.out.println(operacionesMatematicas.operacionMultiplicacion(11,5));
+        System.out.println(operacionesMatematicas.operacionDivision(120,5));
+
     }
 }
--- src/main/java/org/example/operacionesMatematicas.java
+++ src/main/java/org/example/operacionesMatematicas.java
@@ -6,4 +6,18 @@ public class operacionesMatematicas {
        // return "El resultado de la suma de los números es: " + resultadoSuma;
         return "El resultado de la suma de los números es: " + (num1+num2); // aquí va a imprimir 8, porque está considerando el valor de los parentesis como operacion matematica
     }
+    public static String operacionResta(int num1, int num2) {
+        // return "El resultado de la resta de los números es: " + resultadoResta;
+        return "El resultado de la resta de los números es: " + (num1-num2);
+    }
+    public static String operacionMultiplicacion(int num1, int num2) {
+        // return "El resultado de la multiplicacion de los números es: " + resultadoMultiplicacion;
+        return "El resultado de la multiplicacion de los números es: " + (num1*num2);
+    }
+    public static String operacionDivision(int num1, int num2) {
+        // return "El resultado de la division de los números es: " + resultadoDivision;
+        return "El resultado de la division de los números es: " + (num1/num2);
+    }
+
+
 }
