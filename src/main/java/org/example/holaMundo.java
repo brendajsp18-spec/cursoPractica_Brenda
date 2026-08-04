56ea72e8273cc570613f70bf76c25c777b71b1d6
Salazar@7QWNVV3 <brenda.jsp18@gmail.com>
2026-08-04 13:13:00 -0600

    Cierro autenticacion

--- src/main/java/org/example/holaMundo.java
+++ src/main/java/org/example/holaMundo.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class holaMundo {
     public static void main (String[] args){
-        System.out.println("Hola mundo, mi nombre es Brenda :) ");
+        System.out.println("Hola mundo, mi nombre es Brenda Salazar :) ");
     }
 }
